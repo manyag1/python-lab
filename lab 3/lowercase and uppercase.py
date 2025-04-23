407eae339f28bfc4ece1e7e62cdcd5288d4f0969
manyag1 <24bit074@sot.pdpu.ac.in>
2025-04-23 14:47:24 +0530

    Add files via upload

---
+++ lab 3/lowercase and uppercase.py
@@ -0,0 +1,23 @@
+def sm():
+    for i in range(97,123):
+        print(chr(i), end=" ")
+
+sm()
+print("\n")
+def cap():
+    for i in range(65, 91):
+        print(chr(i), end=" ")
+cap()    
+
+
+
+
+
+
+
+
+
+
+
+
+
